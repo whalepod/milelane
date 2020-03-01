0a3202badce2169f480d3854341bef0488075cab
MasashiFujiike <masashi.fujiike@gmail.com>
2020-03-02 00:02:22 +0900

    Added device domain Create() irregular test.

--- app/domain/device_test.go
+++ app/domain/device_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/whalepod/milelane/app/domain/repository"
+	"golang.org/x/xerrors"
 )
 
 type DeviceAccessorMock struct{}
@@ -18,7 +19,22 @@ func (*DeviceAccessorMock) Create(deviceID string, deviceType uint) (*repository
 	}, nil
 }
 
-func TestCreateDevice(t *testing.T) {
+type DeviceAccessorErrorMock struct{}
+
+func (*DeviceAccessorErrorMock) Create(deviceID string, deviceType uint) (*repository.Device, error) {
+	return nil, xerrors.New("Error mock called.")
+}
+
+var deviceTypeStrs = []string{
+	"desktop",
+	"smartphone",
+	"tablet",
+	"ios",
+	"android",
+	"other",
+}
+
+func TestDeviceCreate(t *testing.T) {
 	var deviceAccessor DeviceAccessorMock
 	device, err := NewDevice(&deviceAccessor)
 	if err != nil {
@@ -26,11 +42,52 @@ func TestCreateDevice(t *testing.T) {
 	}
 
 	deviceID := "dc625158-a9e9-4b7c-b15a-89991b013147"
-	deviceType := "desktop"
 
-	_, err = device.Create(deviceID, deviceType)
+	for _, d := range deviceTypeStrs {
+		_, err = device.Create(deviceID, d)
+		if err != nil {
+			t.Fatalf("Returned err response: %s", err.Error())
+		}
+	}
+
+	t.Log("Success.")
+}
+
+func TestDeviceCreateError(t *testing.T) {
+	var deviceAccessor DeviceAccessorErrorMock
+	device, err := NewDevice(&deviceAccessor)
 	if err != nil {
 		t.Fatalf("Returned err response: %s", err.Error())
+	}
+
+	deviceID := ""
+	deviceType := "desktop"
+	_, err = device.Create(deviceID, deviceType)
+	if err.Error() != "Error mock called." {
+		t.Fatalf("Got %v\nwant %v", err, "Error mock called.")
+	}
+
+	t.Log("Success: Got expected err.")
+}
+
+var deviceTypes = []struct {
+	in  DeviceType
+	out string
+}{
+	{TypeDesktop, "desktop"},
+	{TypeSmartphone, "smartphone"},
+	{TypeTablet, "tablet"},
+	{TypeIOS, "ios"},
+	{TypeAndroid, "android"},
+	{999, "undefined"},
+}
+
+func TestDeviceTypeString(t *testing.T) {
+	for _, d := range deviceTypes {
+		deviceType := d.in
+		if deviceType.String() != d.out {
+			t.Fatalf("DeviceType conversion to string failed, got response: %s", deviceType.String())
+		}
 	}
 
 	t.Log("Success.")
