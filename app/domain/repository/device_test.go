93bba63960b914c300175de4b46ea1804115106f
MasashiFujiike <masashi.fujiike@gmail.com>
2020-03-01 23:42:45 +0900

    Added irregular test for DeviceRepository.Create().

--- app/domain/repository/device_test.go
+++ app/domain/repository/device_test.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"fmt"
 	"regexp"
 	"testing"
 
@@ -28,6 +29,45 @@ func TestCreateDevice(t *testing.T) {
 	_, err := deviceRepository.Create(deviceID, deviceType)
 	if err != nil {
 		t.Fatalf("Returned err response: %s", err.Error())
+	}
+
+	t.Log("Success.")
+}
+
+func TestCreateDeviceWithoutDeviceID(t *testing.T) {
+	db, _, _ := getDBMock()
+	defer db.Close()
+
+	deviceRepository := NewDevice(db)
+
+	// Set blank title.
+	deviceID := ""
+	var deviceType uint = 0
+	_, err := deviceRepository.Create(deviceID, deviceType)
+	if err.Error() != "DeviceID can't have blank value." {
+		t.Fatalf("Got %v\nwant %v", err, "DeviceID can't have blank value.")
+	}
+
+	t.Log("Success.")
+}
+
+func TestCreateDeviceRollback(t *testing.T) {
+	db, mock, _ := getDBMock()
+	defer db.Close()
+
+	mock.ExpectBegin()
+
+	mock.ExpectExec(regexp.QuoteMeta(QueryDeviceInsert)).
+		WillReturnError(fmt.Errorf("Device insertion failed."))
+
+	mock.ExpectRollback()
+
+	deviceRepository := NewDevice(db)
+	deviceID := "561c36de-695d-49e8-b124-57e1f742c90a"
+	var deviceType uint = 0
+	_, err := deviceRepository.Create(deviceID, deviceType)
+	if err.Error() != "Device insertion failed." {
+		t.Fatalf("Got %v\nwant %v", err, "Device insertion failed.")
 	}
 
 	t.Log("Success.")
