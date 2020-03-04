4db330f78e7ef1f3098521ee4eb2c8ad4ba68dd1
MasashiFujiike <masashi.fujiike@gmail.com>
2020-03-04 20:59:08 +0900

    Added device_tasks table to schema and seed.

--- db/schema.sql
+++ db/schema.sql
@@ -30,3 +30,14 @@ CREATE TABLE IF NOT EXISTS `task_relations` (
 
 ALTER TABLE `task_relations` ADD INDEX `idx_task_relation_ancestor_id`(`ancestor_id`);
 ALTER TABLE `task_relations` ADD INDEX `idx_task_relation_descendant_id`(`descendant_id`);
+
+CREATE TABLE IF NOT EXISTS `device_tasks` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `device_id` VARCHAR(36) NOT NULL,
+  `task_id` INT UNSIGNED NOT NULL,
+  `created_at` DATETIME NOT NULL,
+  `updated_at` DATETIME NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE `device_tasks` ADD INDEX `idx_device_task_device_id`(`device_id`);
+ALTER TABLE `device_tasks` ADD INDEX `idx_device_task_task_id`(`task_id`);
