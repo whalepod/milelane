b8f8a15faefd3903e944739728c8bc61ac0debe2
MasashiFujiike <masashi.fujiike@gmail.com>
2020-05-06 17:38:38 +0900

    Changed loading timezone into JST.

--- app/domain/task.go
+++ app/domain/task.go
@@ -71,18 +71,18 @@ func (t *Task) List() (*[]Task, error) {
 			ID:        treeableTask.ID,
 			Title:     treeableTask.Title,
 			Type:      TaskType(treeableTask.Type).String(),
-			CreatedAt: treeableTask.CreatedAt.Format("2006-01-02 15:04:05"),
-			UpdatedAt: treeableTask.UpdatedAt.Format("2006-01-02 15:04:05"),
+			CreatedAt: treeableTask.CreatedAt.In(time.Local).Format("2006-01-02 15:04:05"),
+			UpdatedAt: treeableTask.UpdatedAt.In(time.Local).Format("2006-01-02 15:04:05"),
 			Depth:     treeableTask.Depth,
 		}
 		if treeableTask.CompletedAt != nil {
-			task.CompletedAt = treeableTask.CompletedAt.Format("2006-01-02 15:04:05")
+			task.CompletedAt = treeableTask.CompletedAt.In(time.Local).Format("2006-01-02 15:04:05")
 		}
 		if treeableTask.StartsAt != nil {
-			task.StartsAt = treeableTask.StartsAt.Format("2006-01-02 15:04:05")
+			task.StartsAt = treeableTask.StartsAt.In(time.Local).Format("2006-01-02 15:04:05")
 		}
 		if treeableTask.ExpiresAt != nil {
-			task.ExpiresAt = treeableTask.ExpiresAt.Format("2006-01-02 15:04:05")
+			task.ExpiresAt = treeableTask.ExpiresAt.In(time.Local).Format("2006-01-02 15:04:05")
 		}
 		task = task.mapChildren(treeableTask.Children)
 		tasks = append(tasks, task)
@@ -103,18 +103,18 @@ func (t *Task) Find(id uint) (*Task, error) {
 		ID:        treeableTask.ID,
 		Title:     treeableTask.Title,
 		Type:      TaskType(treeableTask.Type).String(),
-		CreatedAt: treeableTask.CreatedAt.Format("2006-01-02 15:04:05"),
-		UpdatedAt: treeableTask.UpdatedAt.Format("2006-01-02 15:04:05"),
+		CreatedAt: treeableTask.CreatedAt.In(time.Local).Format("2006-01-02 15:04:05"),
+		UpdatedAt: treeableTask.UpdatedAt.In(time.Local).Format("2006-01-02 15:04:05"),
 		Depth:     treeableTask.Depth,
 	}
 	if treeableTask.CompletedAt != nil {
-		task.CompletedAt = treeableTask.CompletedAt.Format("2006-01-02 15:04:05")
+		task.CompletedAt = treeableTask.CompletedAt.In(time.Local).Format("2006-01-02 15:04:05")
 	}
 	if treeableTask.StartsAt != nil {
-		task.StartsAt = treeableTask.StartsAt.Format("2006-01-02 15:04:05")
+		task.StartsAt = treeableTask.StartsAt.In(time.Local).Format("2006-01-02 15:04:05")
 	}
 	if treeableTask.ExpiresAt != nil {
-		task.ExpiresAt = treeableTask.ExpiresAt.Format("2006-01-02 15:04:05")
+		task.ExpiresAt = treeableTask.ExpiresAt.In(time.Local).Format("2006-01-02 15:04:05")
 	}
 	task = task.mapChildren(treeableTask.Children)
 
@@ -128,18 +128,18 @@ func (t *Task) mapChildren(treeableTasks []repository.TreeableTask) Task {
 			ID:        treeableTask.ID,
 			Title:     treeableTask.Title,
 			Type:      TaskType(treeableTask.Type).String(),
-			CreatedAt: treeableTask.CreatedAt.Format("2006-01-02 15:04:05"),
-			UpdatedAt: treeableTask.UpdatedAt.Format("2006-01-02 15:04:05"),
+			CreatedAt: treeableTask.CreatedAt.In(time.Local).Format("2006-01-02 15:04:05"),
+			UpdatedAt: treeableTask.UpdatedAt.In(time.Local).Format("2006-01-02 15:04:05"),
 			Depth:     treeableTask.Depth,
 		}
 		if treeableTask.CompletedAt != nil {
-			task.CompletedAt = treeableTask.CompletedAt.Format("2006-01-02 15:04:05")
+			task.CompletedAt = treeableTask.CompletedAt.In(time.Local).Format("2006-01-02 15:04:05")
 		}
 		if treeableTask.StartsAt != nil {
-			task.StartsAt = treeableTask.StartsAt.Format("2006-01-02 15:04:05")
+			task.StartsAt = treeableTask.StartsAt.In(time.Local).Format("2006-01-02 15:04:05")
 		}
 		if treeableTask.ExpiresAt != nil {
-			task.ExpiresAt = treeableTask.ExpiresAt.Format("2006-01-02 15:04:05")
+			task.ExpiresAt = treeableTask.ExpiresAt.In(time.Local).Format("2006-01-02 15:04:05")
 		}
 		task = task.mapChildren(treeableTask.Children)
 		children = append(children, task)
@@ -160,8 +160,8 @@ func (t *Task) Create(title string) (*Task, error) {
 		ID:        (*repositoryTask).ID,
 		Title:     (*repositoryTask).Title,
 		Type:      TaskType((*repositoryTask).Type).String(),
-		CreatedAt: (*repositoryTask).CreatedAt.Format("2006-01-02 15:04:05"),
-		UpdatedAt: (*repositoryTask).UpdatedAt.Format("2006-01-02 15:04:05"),
+		CreatedAt: (*repositoryTask).CreatedAt.In(time.Local).Format("2006-01-02 15:04:05"),
+		UpdatedAt: (*repositoryTask).UpdatedAt.In(time.Local).Format("2006-01-02 15:04:05"),
 		Depth:     1,
 	}
 
