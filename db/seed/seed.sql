f4eaea4ab50f21c69dc8b5f0a07733a46176ebf9
sanoyo <sa00380304yo@gmail.com>
2021-02-23 19:00:04 +0900

    Add devices Table

--- db/seed/seed.sql
+++ db/seed/seed.sql
@@ -1,4 +1,4 @@
-USE `milelane`;
+USE milelane;
 
 -- Avoid 文字化け of Japanese words when inserting data.
 SET CHARACTER_SET_CLIENT = utf8mb4;
@@ -22,6 +22,21 @@ INSERT INTO notes (
   updated_at
 ) VALUES (
   'タイトルなしのノートの本文',
+  NOW(),
+  NOW()
+);
+
+INSERT INTO devices (
+  uuid,
+  device_token,
+  type,
+  created_at,
+  updated_at
+)
+VALUES (
+  '60982a48-9328-441f-805b-d3ab0cad9e1f',
+  'dc625158-a9e9-4b7c-b15a-89991b013147',
+  0,
   NOW(),
   NOW()
 );
