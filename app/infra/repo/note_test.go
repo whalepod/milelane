a52b3b5b7fc94d0b55ee2ad3e7499ec55717b29a
sanoyo <sa00380304yo@gmail.com>
2021-04-04 09:40:52 +0900

    Apply Sql Mock in repo

--- app/infra/repo/note_test.go
+++ app/infra/repo/note_test.go
@@ -1,8 +1,12 @@
 package repo_test
 
 import (
+	"fmt"
 	"testing"
+	"time"
 
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/jmoiron/sqlx"
 	"github.com/whalepod/milelane/app/infra"
 	"github.com/whalepod/milelane/app/infra/repo"
 )
@@ -12,31 +16,50 @@ func TestNotesCreate(t *testing.T) {
 		name    string
 		title   string
 		body    string
-		wantErr error
+		DBError bool
+		wantErr string
 	}{
 		{
-			name:    "success",
+			name:    "Success",
 			title:   "test title",
 			body:    "test body",
-			wantErr: nil,
+			DBError: false,
+			wantErr: "",
+		},
+		{
+			name:    "Fail",
+			title:   "test title",
+			body:    "test body",
+			DBError: true,
+			wantErr: "DB error",
 		},
 	}
 
 	noteRepository := repo.NewNote(infra.DB)
 	for _, tt := range tests {
+		originalDB := infra.DB
+
 		t.Run(tt.name, func(t *testing.T) {
-			err := noteRepository.Create(tt.title, tt.body)
-			if tt.wantErr != nil {
-				if err == nil {
-					t.Error("Un expected error, but got nil")
-					return
-				}
+			if tt.DBError {
+				db, mock, _ := sqlmock.New()
+				defer db.Close()
+
+				mock.ExpectPrepare("INSERT INTO").
+					WillBeClosed().
+					ExpectExec().
+					WithArgs(tt.title, tt.body, time.Now(), time.Now()).
+					WillReturnError(fmt.Errorf("DB error"))
+
+				infra.DB = sqlx.NewDb(db, "sqlmock")
 			}
 
-			if err != nil {
-				t.Errorf("err should be nil, but got %q", err)
-				return
+			noteRepository = repo.NewNote(infra.DB)
+			err := noteRepository.Create(tt.title, tt.body)
+			if err != nil && err.Error() != tt.wantErr {
+				t.Errorf("expected %s but got %s", tt.wantErr, err.Error())
 			}
+
+			infra.DB = originalDB
 		})
 	}
 }
