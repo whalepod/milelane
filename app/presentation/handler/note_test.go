f20c00af1a93f14bfa184ac047ffd990bbfe7034
sanoyo <sa00380304yo@gmail.com>
2021-04-10 09:58:55 +0900

    Add Note Handler Test

--- app/presentation/handler/note_test.go
+++ app/presentation/handler/note_test.go
@@ -5,47 +5,71 @@ import (
 	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"regexp"
 	"testing"
 
+	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/gin-gonic/gin"
 	"github.com/google/go-cmp/cmp"
+	"github.com/whalepod/milelane/app/infra"
 	"github.com/whalepod/milelane/app/presentation/handler"
 )
 
 func TestNoteCreate(t *testing.T) {
 	tests := []struct {
-		name     string
-		title    string
-		body     string
-		DBError  bool
-		wantCode int
+		name                string
+		title               string
+		body                string
+		DBError             bool
+		wantCode            int
+		shouldInjectDBError bool
 	}{
 		{
-			name:     "Success",
-			title:    "test title",
-			body:     "test body",
-			DBError:  false,
-			wantCode: http.StatusOK,
+			name:                "Success",
+			title:               "test title",
+			body:                "test body",
+			DBError:             false,
+			wantCode:            http.StatusOK,
+			shouldInjectDBError: false,
 		},
 		{
-			name:     "Success(Empty title)",
-			title:    "",
-			body:     "test body",
-			DBError:  false,
-			wantCode: http.StatusOK,
+			name:                "Success(Empty title)",
+			title:               "",
+			body:                "test body",
+			DBError:             false,
+			wantCode:            http.StatusOK,
+			shouldInjectDBError: false,
 		},
 		{
-			name:     "Fail(binding error)",
-			title:    "",
-			body:     "",
-			DBError:  false,
-			wantCode: http.StatusUnprocessableEntity,
+			name:                "Fail(binding error)",
+			title:               "",
+			body:                "",
+			DBError:             false,
+			wantCode:            http.StatusUnprocessableEntity,
+			shouldInjectDBError: false,
+		},
+		{
+			name:                "Fail(DB error)",
+			title:               "",
+			body:                "",
+			DBError:             false,
+			wantCode:            http.StatusUnprocessableEntity,
+			shouldInjectDBError: true,
 		},
 	}
 
 	for _, tt := range tests {
 		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
+			originalDB := infra.DB
+			if tt.shouldInjectDBError {
+				db, mock, _ := sqlmock.New()
+				defer db.Close()
+
+				mock.ExpectExec(regexp.QuoteMeta(`INSERT INTO "notes" ("title", "body", "created_at", "updated_at") VALUES (?,?,NOW(),NOW());`)).
+					WillReturnError(fmt.Errorf("DB error"))
+			}
+
 			// Create Receive
 			rec := httptest.NewRecorder()
 			_, r := gin.CreateTestContext(rec)
@@ -64,6 +88,8 @@ func TestNoteCreate(t *testing.T) {
 			if diff := cmp.Diff(tt.wantCode, rec.Code); diff != "" {
 				t.Errorf("mismatch body (-want +got):\n%s", diff)
 			}
+
+			infra.DB = originalDB
 		})
 	}
 }
